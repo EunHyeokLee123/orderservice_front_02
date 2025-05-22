42457c063d165138f1579599f565e96a3a083034
EunHyeok <secun77@naver.com>
2025-05-22 14:18:13 +0900

    수정

--- src/components/MemberCreate.jsx
+++ src/components/MemberCreate.jsx
@@ -9,7 +9,7 @@ import {
 } from '@mui/material';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { API_BASE_URL } from '../configs/host-config';
+import { API_BASE_URL, USER } from '../configs/host-config';
 
 const MemberCreate = () => {
   const [name, setName] = useState('');
